bdfbe4900cb98b07cc7f7e0add344db315c71b40
KrunalGurao <krunalgurao1@gmail.com>
2022-12-08 19:09:12 +0530

    please fix the bug

---
+++ prime_code.js
@@ -0,0 +1,16 @@
+Prime No. :-
+
+    let count=13;
+    for (let i=0; i<=num;i++)
+    {
+        if (count%i==0)
+        {
+            count;
+        }
+    }
+    if (count>2)
+    {
+        console.log("Yes");
+    }
+    else
+    console.log("No");
